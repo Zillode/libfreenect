0bb45f00003ff170b5a7de72d6bd504abfa446f0
Lode Hoste <zillode@zillode.be>
2011-07-24 04:00:11 +0200

    Updated Ruby FFI. Currently segfaults when calling freenect_set_video_mode and freenect_set_depth_mode.

--- wrappers/ruby/ffi-libfreenect/lib/ffi/freenect.rb
+++ wrappers/ruby/ffi-libfreenect/lib/ffi/freenect.rb
@@ -9,47 +9,28 @@ require 'ffi'
 module FFI::Freenect
   extend FFI::Library
   ffi_lib 'freenect', 'freenect_sync'
-  
-  FRAME_W = 640
-  FRAME_H = 480
-  FRAME_PIX = FRAME_W * FRAME_H
 
-  IR_FRAME_W = 640
-  IR_FRAME_H = 488
-  IR_FRAME_PIX = FRAME_W * FRAME_H
- 
-  RGB_SIZE = FRAME_PIX * 3
-  BAYER_SIZE = FRAME_PIX
-  YUV_RGB_SIZE = RGB_SIZE
-  YUV_RAW_SIZE = FRAME_PIX * 2
-  IR_8BIT_SIZE = IR_FRAME_PIX
-  IR_10BIT_SIZE = IR_FRAME_PIX * 2
-  IR_10BIT_PACKED_SIZE = 390400
 
-  DEPTH_11BIT_SIZE = FRAME_PIX * 2
-  DEPTH_10BIT_SIZE = DEPTH_11BIT_SIZE
-  DEPTH_11BIT_PACKED_SIZE = 422400
-  DEPTH_10BIT_PACKED_SIZE = 384000
+	COUNTS_PER_G = 819
 
-  COUNTS_PER_G = 819
+	DEVICE_MOTOR = 0x01
+	DEVICE_CAMERA = 0x02
+	DEVICE_AUDIO = 0x04
 
-	LED_OFF    = 0
-	LED_GREEN  = 1
-	LED_RED    = 2
-	LED_YELLOW = 3
-	LED_BLINK_YELLOW = 4
-	LED_BLINK_GREEN = 5
-	LED_BLINK_RED_YELLOW = 6
+	DEVICE_FLAGS = enum( :device_motor,		DEVICE_MOTOR,
+											 :device_camera,	DEVICE_CAMERA,
+											 :device_audio,		DEVICE_AUDIO)
 
-  LED_OPTIONS = enum( :off,               LED_OFF,
-                      :green,             LED_GREEN,
-                      :red,               LED_RED,
-                      :yellow,            LED_YELLOW,
-                      :blink_yellow,      LED_BLINK_YELLOW,
-                      :blink_green,       LED_BLINK_GREEN,
-                      :blink_red_yellow,  LED_BLINK_RED_YELLOW) 
- 
- 
+	RESOLUTION_LOW = 0
+	RESOLUTION_MEDIUM = 1
+	RESOLUTION_HIGH = 2
+	RESOLUTION_DUMMY = 2147483647
+
+	RESOLUTIONS = enum( :resolution_low,		RESOLUTION_LOW,
+										 :resolution_medium,	RESOLUTION_MEDIUM,
+										 :resolution_high,		RESOLUTION_HIGH,
+										 :resolution_dummy,		RESOLUTION_DUMMY)
+
 	VIDEO_RGB = 0
 	VIDEO_BAYER = 1
 	VIDEO_IR_8BIT = 2
@@ -57,38 +38,66 @@ module FFI::Freenect
 	VIDEO_IR_10BIT_PACKED = 4
 	VIDEO_YUV_RGB = 5
 	VIDEO_YUV_RAW = 6
+	VIDEO_DUMMY = 2147483647
 
-  VIDEO_FORMATS = enum( :rgb,             VIDEO_RGB,
-                        :bayer,           VIDEO_BAYER,
-                        :ir_8bit,         VIDEO_IR_8BIT,
-                        :ir_10bit,        VIDEO_IR_10BIT,
-                        :yuv_rgb,         VIDEO_YUV_RGB,
-                        :yuv_raw,         VIDEO_YUV_RAW,
-                        :ir_10bit_packed, VIDEO_IR_10BIT_PACKED)
-  
-  VIDEO_SIZES = enum( :rgb,             RGB_SIZE,
-                      :bayer,           BAYER_SIZE,
-                      :ir_8bit,         IR_8BIT_SIZE,
-                      :ir_10bit,        IR_10BIT_SIZE,
-                      :yuv_rgb,         YUV_RGB_SIZE,
-                      :yuv_raw,         YUV_RAW_SIZE,
-                      :ir_10bit_packed, IR_10BIT_PACKED_SIZE )
+  VIDEO_FORMATS = enum( :rgb,         VIDEO_RGB,
+                       :bayer,        VIDEO_BAYER,
+                       :ir_8bit,      VIDEO_IR_8BIT,
+                       :ir_10,				VIDEO_IR_10BIT,
+                       :ir_10bit,     VIDEO_IR_10BIT,
+                       :yuv_rgb,      VIDEO_YUV_RGB,
+                       :yuv_raw,      VIDEO_YUV_RAW,
+											 :video_dummy,	VIDEO_DUMMY)
 
- 
 	DEPTH_11BIT = 0
 	DEPTH_10BIT = 1
 	DEPTH_11BIT_PACKED = 2
 	DEPTH_10BIT_PACKED = 3
+	DEPTH_DUMMY = 2147483647
 
   DEPTH_FORMATS = enum( :depth_11bit,         DEPTH_11BIT,
-                        :depth_10bit,         DEPTH_10BIT,
-                        :depth_11bit_packed,  DEPTH_11BIT_PACKED,
-                        :depth_10bit_packed,  DEPTH_10BIT_PACKED)
+                       :depth_10bit,          DEPTH_10BIT,
+                       :depth_11bit_packed,   DEPTH_11BIT_PACKED,
+                       :depth_10bit_packed,   DEPTH_10BIT_PACKED,
+											 :depth_dummy,					DEPTH_DUMMY)
 
-  DEPTH_SIZES = enum( :depth_11bit,         DEPTH_11BIT_SIZE,
-                      :depth_10bit,         DEPTH_10BIT_SIZE,
-                      :depth_11bit_packed,  DEPTH_11BIT_PACKED_SIZE,
-                      :depth_10bit_packed,  DEPTH_10BIT_PACKED_SIZE )
+	class VideoOrDepthFormat < FFI::Union
+		layout	:dummy,					:int32_t,
+						:video_format,	VIDEO_FORMATS,
+						:depth_format,	DEPTH_FORMATS
+	end
+
+  class FrameMode < FFI::Struct
+    layout :reserved,								:int32_t,
+           #:resolution,							RESOLUTIONS, # TODO
+           :resolution,							:int32_t,
+					 #:video_or_depth_format,	VideoOrDepthFormat, # TODO
+					 :video_or_depth_format,  :int32_t,
+					 :bytes,									:int32_t,
+					 :width,									:int16_t,
+					 :height,									:int16_t,
+					 :data_bits_per_pixel,		:int8_t,
+					 :padding_bits_per_pixel,	:int8_t,
+					 :framerate,							:int8_t,
+					 :is_valid,								:int8_t
+  end
+
+
+	LED_OFF    = 0
+	LED_GREEN  = 1
+	LED_RED    = 2
+	LED_YELLOW = 3
+	LED_BLINK_GREEN = 4
+	# 5 is same as 4, LED blink Green
+	LED_BLINK_RED_YELLOW = 6
+
+  LED_OPTIONS = enum( :off,               LED_OFF,
+                      :green,             LED_GREEN,
+                      :red,               LED_RED,
+                      :yellow,            LED_YELLOW,
+                      :blink_green,       LED_BLINK_GREEN,
+                      :blink_red_yellow,  LED_BLINK_RED_YELLOW)
+
 
 	TILT_STATUS_STOPPED = 0x00
 	TILT_STATUS_LIMIT = 0x01
@@ -98,10 +107,22 @@ module FFI::Freenect
                             :limit,    TILT_STATUS_LIMIT,
                             :moving,   TILT_STATUS_MOVING)
 
- 
+
+	class RawTiltState < FFI::Struct
+    layout :accelerometer_x,  :int16_t,
+           :accelerometer_y,  :int16_t,
+           :accelerometer_z,  :int16_t,
+           :tilt_angle,       :int8_t,
+           :tilt_status,      TILT_STATUS_CODES
+  end
+
+	typedef :pointer, :freenect_context
+  typedef :pointer, :freenect_device
+  typedef :pointer, :freenect_usb_context # actually a libusb_context
+
 	LOG_FATAL = 0
 	LOG_ERROR = 1
-	LOG_WARNING = 2 
+	LOG_WARNING = 2
 	LOG_NOTICE = 3
 	LOG_INFO = 4
 	LOG_DEBUG = 5
@@ -116,40 +137,26 @@ module FFI::Freenect
                     :debug,   LOG_DEBUG,
                     :spew,    LOG_SPEW,
                     :flood,   LOG_FLOOD)
-  
-  typedef :pointer, :freenect_context
-  typedef :pointer, :freenect_device
-  typedef :pointer, :freenect_usb_context # actually a libusb_context
- 
- 
-  class RawTiltState < FFI::Struct
-    layout :accelerometer_x,  :int16_t,
-           :accelerometer_y,  :int16_t,
-           :accelerometer_z,  :int16_t, 
-           :tilt_angle,       :int8_t, 
-           :tilt_status,      TILT_STATUS_CODES
-  end
 
+
+  attach_function :freenect_init, [:freenect_context, :freenect_usb_context], :int
+  attach_function :freenect_shutdown, [:freenect_context], :int
   callback :freenect_log_cb, [:freenect_context, LOGLEVELS, :string], :void
-  callback :freenect_depth_cb, [:freenect_device, :pointer, :uint32], :void
-  callback :freenect_video_cb, [:freenect_device, :pointer, :uint32], :void
-
   attach_function :freenect_set_log_level, [:freenect_context, LOGLEVELS], :void
   attach_function :freenect_set_log_callback, [:freenect_context, :freenect_log_cb], :void
   attach_function :freenect_process_events, [:freenect_context], :int
   attach_function :freenect_num_devices, [:freenect_context], :int
+  attach_function :freenect_select_subdevices, [:freenect_context, DEVICE_FLAGS], :void
   attach_function :freenect_open_device, [:freenect_context, :freenect_device, :int], :int
   attach_function :freenect_close_device, [:freenect_device], :int
-  attach_function :freenect_init, [:freenect_context, :freenect_usb_context], :int
-  attach_function :freenect_shutdown, [:freenect_context], :int
   attach_function :freenect_set_user, [:freenect_device, :pointer], :void
   attach_function :freenect_get_user, [:freenect_device], :pointer
+  callback :freenect_depth_cb, [:freenect_device, :pointer, :uint32], :void
+  callback :freenect_video_cb, [:freenect_device, :pointer, :uint32], :void
   attach_function :freenect_set_depth_callback, [:freenect_device, :freenect_depth_cb], :void
-  attach_function :freenect_set_video_callback, [:freenect_device, :freenect_video_cb], :void  
-  attach_function :freenect_set_depth_format, [:freenect_device, DEPTH_FORMATS], :int
-  attach_function :freenect_set_video_format, [:freenect_device, VIDEO_FORMATS], :int
-  attach_function :freenect_set_depth_buffer, [:freenect_device, :void], :int
-  attach_function :freenect_set_video_buffer, [:freenect_device, :void], :int
+  attach_function :freenect_set_video_callback, [:freenect_device, :freenect_video_cb], :void
+  attach_function :freenect_set_depth_buffer, [:freenect_device, :pointer], :int
+  attach_function :freenect_set_video_buffer, [:freenect_device, :pointer], :int
   attach_function :freenect_start_depth, [:freenect_device], :int
   attach_function :freenect_start_video, [:freenect_device], :int
   attach_function :freenect_stop_depth, [:freenect_device], :int
@@ -158,12 +165,19 @@ module FFI::Freenect
   attach_function :freenect_get_tilt_state, [:freenect_device], RawTiltState
   attach_function :freenect_get_tilt_degs, [:freenect_device], :double
   attach_function :freenect_set_tilt_degs, [:freenect_device, :double], :int
+  attach_function :freenect_get_tilt_status, [RawTiltState], TILT_STATUS_CODES
   attach_function :freenect_set_led, [:freenect_device, LED_OPTIONS], :int
   attach_function :freenect_get_mks_accel, [RawTiltState, :pointer, :pointer, :pointer], :void
-
-  attach_function :freenect_sync_get_video, [:pointer, :pointer, :int, VIDEO_FORMATS], :int
-  attach_function :freenect_sync_get_depth, [:pointer, :pointer, :int, DEPTH_FORMATS], :int
-  attach_function :freenect_sync_stop, [], :void
+  attach_function :freenect_get_video_mode_count, [], :int
+	attach_function :freenect_get_video_mode, [:int], FrameMode
+	attach_function :freenect_get_current_video_mode, [:freenect_device], FrameMode
+	attach_function :freenect_find_video_mode, [RESOLUTIONS, VIDEO_FORMATS], FrameMode
+	attach_function :freenect_set_video_mode, [:freenect_device, FrameMode], :int
+	attach_function :freenect_get_depth_mode_count, [], :int
+	attach_function :freenect_get_depth_mode, [:int], FrameMode
+	attach_function :freenect_get_current_depth_mode, [:freenect_device], FrameMode
+	attach_function :freenect_find_depth_mode, [RESOLUTIONS, DEPTH_FORMATS], FrameMode
+	attach_function :freenect_set_depth_mode, [:freenect_device, FrameMode], :int
 
 end
 
